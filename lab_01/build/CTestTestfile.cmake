fcc352de96b8f7c7489898995a0d5774bb4c4156
Michele Zhadnov <120622137+miSh1Zh@users.noreply.github.com>
2023-11-19 21:12:38 +0300

    Delete build

--- lab_01/build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/mishazhadnov/Desktop/wD/OOP_labs/lab_01/build
-# Build directory: /Users/mishazhadnov/Desktop/wD/OOP_labs/lab_01/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(BASIC "/Users/mishazhadnov/Desktop/wD/OOP_labs/lab_01/build/tests")
-set_tests_properties(BASIC PROPERTIES  _BACKTRACE_TRIPLES "/Users/mishazhadnov/Desktop/wD/OOP_labs/lab_01/build/CMakeLists.txt;19;add_test;/Users/mishazhadnov/Desktop/wD/OOP_labs/lab_01/build/CMakeLists.txt;0;")
